c717b92c0b04029723c3789fbf9891501f15c6a9
BiG A <antoine.salameh.45@gmail.com>
2025-08-03 00:07:54 +0300

    trying different color palette

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css"
 import { Routes } from "./Layout/routes";
 
 function App() {
--- src/components/pages/RightNow/RightNow.tsx
+++ src/components/pages/RightNow/RightNow.tsx
@@ -1,15 +1,15 @@
 const RightNow = () => {
   return (
-    <div>
-      <h2 className="text-xl font-bold mb-4">Hey...</h2>
-      <p className="mb-2">
+    <div className="bg-pagebg">
+      <h2 className="text-xl font-bold mb-4 text-titles">Hey...</h2>
+      <p className="mb-2 text-paragraph">
         So I know we haven't been on the best terms lately.
         <br />
         It's been a tough shitass week I know. Lots of things happened,
         and a lot more to come. But our story wasn't always like that...
       </p>
-      <h2>Ouverture des rideaux</h2>
-      <p className="mb-2">
+      <h2 className="text-xl font-bold mb-4 text-titles">Ouverture des rideaux</h2>
+      <p className="mb-2 text-paragraph">
         It all started on the genshin group, before that all I new is this girl likes to call me sophia oddly enough.
         I was still new and asking lots of questions and you were there to answer them. I went into your dms 
         to ask you about characters and team comps lol. Idk how but we started co-oping, 
@@ -17,8 +17,8 @@ const RightNow = () => {
         then we turned ou to be neighbors, and I was like "ah kool". Next thing I know you went like
         let's meetup tomorrow and Mr.Brown. I lowkey panicked cause wdym a girl asked me to hangout???
         </p>
-        <h3>Fast forward the next day</h3>
-        <p>
+        <h3 className="text-xl font-bold mb-4 text-titles">Fast forward the next day</h3>
+        <p className="mb-2 text-paragraph">
           How will I know her? How does she look like? Where is she? Then you appeared and i was like
           "Kawaii". I was nervously yapping in a way cause idk u were cute i was weird and didn't know
           what to do. Our first meeting was brief but kool i immediatly wanted to see you again.
@@ -27,8 +27,8 @@ const RightNow = () => {
           you were different.
           We yapped and yapped then it was go-time. 
         </p>
-        <h3>Uni and atcl hangouts</h3>
-        <p>
+        <h3 className="text-xl font-bold mb-4 text-titles">Uni and atcl hangouts</h3>
+        <p className="mb-2 text-paragraph">
           You still had uni and I was enrolled in the bootcamp so we didn't hangout much but we were still
           chatting and playing genshin. Signs appeared along the way of how i was getting hooked, attached,
           I was a little too clingy. It showed something was up. Back then it was maybe just cz u were nice
@@ -39,16 +39,16 @@ const RightNow = () => {
           I felt so small cause i was literally getting rejected before even trying. But you made it clear and import PropTypes from 'prop-types'
           was for the best to the both of us. I tried.. I really did but.. I couldn't help it.
         </p>
-        <h3>We still hung out and stayed fremds</h3>
-        <p>
+        <h3 className="text-xl font-bold mb-4 text-titles">We still hung out and stayed fremds</h3>
+        <p className="mb-2 text-paragraph">
           We kept chatting and yapping and playing and going out. I met your dad then other members of the family.
           I felt accepted in a way even tho we're just friends. You know i was giggling about the bf allegations
           while you were like "nu uh we're just friends". I was like "kool, i can live with that".
           But for how long actually? Every time i was looking at you like "damn she's cute". I didn't even 
           need to speak it was all obvious. I was getting attached and i knew it.
         </p>
-        <h3>Haru Matsuri</h3>
-        <p>
+        <h3 className="text-xl font-bold mb-4 text-titles">Haru Matsuri</h3>
+        <p className="mb-2 text-paragraph">
           It was the first time we go out for a whole day after i got to know you a bit more and understand
           your condition just a little bit. On that day we were having fun until it became too mcuh for you.
           It was the first time i see you like that, I just didn't know what to do and how to help. i made
@@ -63,8 +63,8 @@ const RightNow = () => {
           around you, I always want to play and yap. I just can't help it, I like you a lot. I know it's not easy
           for you to deal with me and my clinginess but I hope you can forgive me for that.
         </p>
-        <h3>Where we are now</h3>
-        <p>
+        <h3 className="text-xl font-bold mb-4 text-titles">Where we are now</h3>
+        <p className="mb-2 text-paragraph">
           The day we went fishing was just like any other day. We spent time together not knowing what was gonna 
           happen. I was just happy being with you and enjoying our time. I got a bit tired and sleepy so i layed 
           down then after some time you had enough of it and stopped fishing. We talked like a lot, I won't go
